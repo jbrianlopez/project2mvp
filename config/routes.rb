68e82f5d71e01592d5b525c2389c7a3ffaa8d51d
Brian Lopez <jbrianlopez@gmail.com>
2016-06-27 11:56:14 +0800

    add gem for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
+  # resources :posts
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
 #
-# resources :users do
-#   resources :posts
+resources :users do
+  resources :posts
+end
+end
