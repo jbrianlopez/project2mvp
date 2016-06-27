b74a5735c60a2ab2206f65a78ff3708a22bcd7f6
Brian Lopez <jbrianlopez@gmail.com>
2016-06-27 11:00:05 +0800

    passed all model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  has_secure_password
+  validates :password, confirmation: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true,
+  format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
 end
