1064bb3936a703faa3ed82cfdbdbdd7ae3626ddb
Brian Lopez <jbrianlopez@gmail.com>
2016-06-29 10:52:38 +0800

    notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :password, confirmation: true, length: { minimum: 8 }
   validates :email, presence: true, uniqueness: true,
   format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+  # dependent :destroy
 end
